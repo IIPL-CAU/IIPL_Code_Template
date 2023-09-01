e666c598b498317e6ff5a1f69823d06cd3071db3
movie112 <112movie@naver.com>
2023-09-01 19:49:49 +0900

    add_set_seed

--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,16 @@
+import os
+import torch
+import numpy as np
 
-
+def set_seed(seed=42):
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    # When running on the CuDNN backend, two further options must be set
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    # Set a fixed value for the hash seed
+    os.environ['PYTHONHASHSEED'] = str(seed)
 
 def list_str2float(data):
     ret = []
