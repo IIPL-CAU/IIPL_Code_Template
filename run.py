22aa8bf5afcc4978dd964f85919e2695908a0f58
fhzh123 <fhzh@naver.com>
2023-09-01 20:09:21 +0900

    init

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from training import training
 from preprocessing import Preprocessing
 from testing import testing
 
-
+# Commit message template test용
 
 if __name__=='__main__':
     args = init_parser()
