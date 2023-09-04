40d0f23348c62eb4014eac915b5c1d36f9a6eb18
movie112 <112movie@naver.com>
2023-09-04 18:52:19 +0900

    add optimizer, scheduler, logger get_optimizer(), get_scheduler(), get_logger() 를 추가하였습니다. feature : utils 함수 추

--- utils/utils.py
+++ utils/utils.py
@@ -1,7 +1,11 @@
 import os
 import torch
 import numpy as np
+import logging
+import argparse
 
+
+# seed setting
 def set_seed(seed=42):
     np.random.seed(seed)
     torch.manual_seed(seed)
@@ -12,6 +16,41 @@ def set_seed(seed=42):
     # Set a fixed value for the hash seed
     os.environ['PYTHONHASHSEED'] = str(seed)
 
+# logger setting
+def get_logger(logger_path:str=None, logger_name:str=None, args:argparse.Namespace=None):
+    ''' 
+    Args:
+        logger_path (str): logger saving path (.log file)
+        logger_name (str): logger name
+
+    logger = get_logger(logger_path, logger_name, args)
+    logger.info("message")
+    '''
+    if logger_path is None:
+        if args is None:
+            raise ValueError("logger_path is None and args is None")
+        else:
+            logger_path = args.logger_path
+    if logger_name is None:
+        if args is None:
+            raise ValueError("logger_name is None and args is None")
+        else:
+            logger_name = args.logger_name
+    # logger file generation        
+    if not os.path.exists(logger_path):
+        os.makedirs(logger_path)
+
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(message)s')
+    
+    file_handler = logging.FileHandler(logger_path)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+    
+    return logger
+
+##??
 def list_str2float(data):
     ret = []
     for s in data:
