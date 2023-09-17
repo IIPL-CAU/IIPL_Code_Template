e104ac8cb2556d45d6a7fa7d976f31831abc0de0
joo98 <joo98@github.com>
2023-09-18 00:59:17 +0900

    Add shell script file for seq2label task
    feature : 새로운 기능 추가

---
+++ scripts/seq2label.sh
@@ -0,0 +1,29 @@
+time=`date +%y-%m-%d-%T`
+train=true 
+test=true 
+model='bert-base-uncased'
+tokenizer='bert-base-uncased'
+TASK='single_text_classification'
+model_path='models/save/'
+dataset_path='imdb'
+
+
+
+for string in ${TASK} ${model} ${data_path} ${time}; do
+    model_path+=${string}
+    model_path+='_'
+done
+model_path+='.pt'
+echo ${model_path}
+
+
+
+if ${train} -eq true; then
+    CUDA_VISIBLE_DEVICES=0 python3 run.py --training --task ${TASK} --model ${model} --tokenizer ${tokenizer} --dataset_path ${dataset_path} --model_path ${model_path}\
+    > results/train/${TASK}_${model}_${data_path}_${time}.log
+fi
+
+if ${test} -eq true; then
+    CUDA_VISIBLE_DEVICES=0 python3 run.py --testing --task ${TASK} --model ${model} --tokenizer ${tokenizer} --dataset_path ${dataset_path}  --model_path ${model_path}\
+    > results/test/${TASK}_${model}_${data_path}_${time}.log 
+fi
